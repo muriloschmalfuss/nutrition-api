a578f75bbcec2b0acdab6448e8b775a4914a9750
Murilo Schmalfuss <murilo.schmalfuss@gmail.com>
2023-07-19 20:19:04 -0300

    cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('*', cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
